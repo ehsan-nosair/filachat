b10fb8b9cae2b255bcb69fe89a1f9521a1028c1c
ehsan nosair <ehsannosair@gmail.com>
2024-11-26 14:27:17 +0300

    modifications for RTL support

--- resources/views/filachat/index.blade.php
+++ resources/views/filachat/index.blade.php
@@ -1,5 +1,5 @@
 <x-filament-panels::page>
-    <div class="flex bg-white shadow-sm rounded-xl ring-1 ring-gray-950/5 dark:divide-white/10 dark:bg-gray-900 dark:ring-white/10"
+    <div dir="ltr" class="flex bg-white shadow-sm rounded-xl ring-1 ring-gray-950/5 dark:divide-white/10 dark:bg-gray-900 dark:ring-white/10"
         style="height: calc(100vh - 8rem);">
         <!-- Left Sidebar (Chat List) -->
         <livewire:filachat-chat-list :selectedConversation="$selectedConversation" />
